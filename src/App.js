1b500523c4a4c394b853bcde251f8a488f410466
Daniel <daniel@zintin.localdomain>
2019-10-29 09:55:20 -0500

    Finished the home page.  Mobile seems to work on most devices.

--- src/App.js
+++ src/App.js
@@ -1,43 +1,56 @@
-import React from 'react';
-import { Reset } from 'styled-reset'
-import {MainTitle,MainContainer,GlobalStyles,BlurBox,BlurContainer,MainBio,ButtonsContainer,TButton,LeftContainer} from './styles';
+import React from "react";
+import { Reset } from "styled-reset";
+import { Row, Col } from "react-flexbox-grid";
+import {
+  MainTitle,
+  MainContainer,
+  GlobalStyles,
+  BlurBox,
+  BlurContainer,
+  MainBio,
+  TButton,
+  ButtonsDialpad
+} from "./styles";
 
+const buttons = ['About', 'Projects', 'Discord', 'Github', 'Email']
+const Rower = {
+  width: '20px'
+}
 function App() {
   return (
     <React.Fragment>
-    <Reset />
-    <GlobalStyles/>
-    <MainContainer>
-    <BlurBox>
-      <BlurContainer>
-      <LeftContainer>
-        <MainTitle>
-          Hey, I'm Topik!
-        </MainTitle>
-        <MainBio>
-          I'm a 15 year old web developer from Chicago with a strong passion for technology and code. I have two years of experience and I know how to make elegant and snappy websites.
-        </MainBio>
-      </LeftContainer>
-        <ButtonsContainer>
-          <TButton>
-            About
-          </TButton>
-          <TButton>
-            Projects
-          </TButton>
-          <TButton>
-            Discord
-          </TButton>
-          <TButton>
-            Github
-          </TButton>
-          <TButton>
-            Email
-          </TButton>
-        </ButtonsContainer>
-      </BlurContainer>
-    </BlurBox>
-    </MainContainer>
+      <Reset />
+      <GlobalStyles />
+      <MainContainer>
+        <BlurBox>
+          <BlurContainer>
+            <Row style={{maxWidth: '80%'}} middle="lg" center="xs" start="lg">
+              <Col xs={12} lg={6} center="lg">
+                <Col center="lg" middle="xs">
+                  <MainTitle>Hey, I'm Topik!</MainTitle>
+                </Col>
+                <Col center="lg" middle="xs">
+                  <MainBio>
+                    I'm a front-end web developer from Chicago with a strong
+                    passion for technology and code.  I have serveral years of experience with making websites for projects and clients.
+                  </MainBio>
+                </Col>
+              </Col>
+              <Col middle="lg" center="lg" lg={6}>
+                <ButtonsDialpad>
+                  <Row center="xs" middle="xs">
+                    {buttons.map(btn => ( 
+                      <Col lg={6} xs={6} key={btn}>
+                        <TButton>{btn}</TButton>
+                      </Col>
+                    ))}
+                  </Row>
+                </ButtonsDialpad>
+              </Col>
+            </Row>
+          </BlurContainer>
+        </BlurBox>
+      </MainContainer>
     </React.Fragment>
   );
 }
--- src/styles.js
+++ src/styles.js
@@ -1,12 +1,20 @@
 import styled from 'styled-components'
 import {createGlobalStyle} from 'styled-components';
 import background from "./images/background.jpg";
-
 export const GlobalStyles = createGlobalStyle`
+html {
+        font-size: 16px;
+        @media (max-width: 640px) {
+            font-size: 13px;
+        }
+    }
 @import url('https://fonts.googleapis.com/css?family=Assistant:400,700,800&display=swap');
 `
+
+
 const size = {
     mobile: '750px',
+    mobileS: '500px',
     tablet: '768px',
     laptop: '1024px',
     laptopL: '1440px',
@@ -14,6 +22,7 @@ const size = {
   }
 export const device = {
     mobile: `(max-width: ${size.mobile})`,
+    mobileS: `(max-width: ${size.mobileS})`,
     tablet: `(min-width: ${size.tablet})`,
     laptop: `(min-width: ${size.laptop})`,
     laptopL: `(min-width: ${size.laptopL})`,
@@ -23,66 +32,39 @@ export const device = {
 export const BlurBox = styled.div`
     backdrop-filter: blur(12px);
     width: 100%;
-    height: 50vh;
+    min-height: 450px;
     border-radius: 25px;
     display: flex;
     justify-content: center;
     align-items: center;
     box-shadow: 0px 0px 11px 0px rgba(0,0,0,0.75);  
     color: #ececec;
-    margin: 20px;
     max-width: 800px;
+    max-height: 50vh;
     @media ${device.mobile}{
         max-height: 87vh;
         min-height: 70vh;
         height: 100%;
+        width: 375px;
+        max-width: 85vw;
     }
 `
 export const BlurContainer = styled.div`
     display: flex;
-    align-items: center;
     justify-content: center;
-    flex-direction: row;
-    padding: 50px;
-    @media ${device.mobile}{
-        flex-direction: column;
-        padding: 20px;
-        text-align: center;
-        height: 100%;
-    }
 `
 export const MainTitle = styled.h1`
     font-weight: 800;
-    font-size: 55px;
+    font-size: 3.4rem;
 `
 export const MainBio = styled.p`
-    width: 80%;
-    padding-top: 12px;
-    font-size: 22px;    
-`
-export const LeftContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: left;
-    flex-direction: column;
-    width: 100%;
-    text-align: left;
-    word-wrap: break-word;
-    @media ${device.mobile}{
-        align-items: center;
-        text-align: center;
-    }
-`
-export const ButtonsContainer = styled.div`
-    padding-top: 15px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-flow: wrap;
-
+    font-size: 1.55rem;
+    margin-top: 13px;
 `
 export const TButton = styled.a`
-    width: 160px;
+    min-width: 5em;
+    max-width: 100%;
+    width: 8.4rem;
     height: 48px;
     font-size: 19px;
     border: 4px solid white;
@@ -93,22 +75,24 @@ export const TButton = styled.a`
     user-select: none;
     cursor: pointer;
     font-weight: 700;
-    margin-right: 7.5px;
-    margin-left: 7.5px;
     margin-top: 15px;
+    margin-left: auto;
+    margin-right: auto;
     &:hover{
         transition: .3s;
         background-color: #fff;
         color: #333;
     }
     &:active{
-        box-shadow:
-        height: 100%; inset 0px 10px 22px -18px rgba(0, 0, 0, 0.45);
+        box-shadow: inset 0px 10px 22px -18px rgba(0, 0, 0, 0.45);
     }
-    @media ${device.mobile}{
-        width: 100px;
+`
+export const ButtonsDialpad = styled.div`
+  width: 360px;
+  margin: auto;
+  @media ${device.mobile}{
+        max-width: 100%;
     }
-    
 `
 export const MainContainer = styled.div`
     font-family: Assistant, sans-serif;
