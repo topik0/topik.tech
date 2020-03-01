f526b41a3888c231838d9a95f7b8a7e6d4d3bfc4
Daniel <daniel@zintin.localdomain>
2020-03-01 14:26:43 -0600

    Changed the medium link

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -11,7 +11,7 @@ import {
   LeftRow
 } from "./styles";
 
-const buttons = [{ label: 'About', url: '/about' }, { label: 'Discord', url: 'https://discord.gg/D5aMzpN', external: true }, { label: 'Github', url: 'https://www.github.com/topik0', external: true }, { label: 'Email', url: 'mailto:topik@topik.tech', external: true }, { label: 'Guides', url: 'https://www.medium.com/topik0', external: true }];
+const buttons = [{ label: 'About', url: '/about' }, { label: 'Discord', url: 'https://discord.gg/D5aMzpN', external: true }, { label: 'Github', url: 'https://www.github.com/topik0', external: true }, { label: 'Email', url: 'mailto:topik@topik.tech', external: true }, { label: 'Guides', url: 'https://medium.com/@topik', external: true }];
 const Home = () => (
   <MainContainer>
     <Helmet>
